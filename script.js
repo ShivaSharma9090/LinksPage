5e0e1dceb7c3d7467f4b204aed4d2fe15db06c8e
Shiva Sharma <Shiva990@outlook.com>
2023-10-17 02:34:43 -0400

    Add files via upload
    1

---
+++ script.js
@@ -0,0 +1,35 @@
+// Add a click event listener to the first YouTube icon (index 0)
+document.getElementsByClassName("fab fa-youtube")[0].addEventListener("click", () => {
+    // Open a new window/tab with the specified YouTube video URL
+    window.open("https://youtu.be/rfscVS0vtbw?si=nXzqBjo-V8GiqphW");
+});
+
+// Add a click event listener to the second YouTube icon (index 1)
+document.getElementsByClassName("fab fa-youtube")[1].addEventListener("click", () => {
+    // Open a new window/tab with the specified YouTube video URL
+    window.open("https://youtu.be/PkZNo7MFNFg?si=moiwGThv-CKOWM4W");
+});
+
+// Add a click event listener to the third YouTube icon (index 2)
+document.getElementsByClassName("fab fa-youtube")[2].addEventListener("click", () => {
+    // Open a new window/tab with the specified YouTube video URL
+    window.open("https://youtu.be/WG5ikvJ2TKA?si=rPQhbIGueILarAX0");
+});
+
+// Add a click event listener to the first YouTube icon (index 0) again
+document.getElementsByClassName("fab fa-youtube")[3].addEventListener("click", () => {
+    // Open a new window/tab with a different YouTube video URL
+    window.open("https://youtu.be/cBpGq-vDr2Y?si=OxvlYJodNW3rfZs6");
+});
+
+// Add a click event listener to the second YouTube icon (index 1) again
+document.getElementsByClassName("fab fa-youtube")[4].addEventListener("click", () => {
+    // Open a new window/tab with a different YouTube video URL
+    window.open("https://youtu.be/TjPFZaMe2yw?si=sJSf2tfaboPR2dzI");
+});
+
+// Add a click event listener to the third YouTube icon (index 2) again
+document.getElementsByClassName("fab fa-youtube")[5].addEventListener("click", () => {
+    // Open a new window/tab with a different YouTube video URL
+    window.open("https://youtu.be/dC4nLdRpIBA?si=Q3cXNdMcP8dp1mPR");
+});
